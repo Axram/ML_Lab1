14b5cba4be115df41410890d8ea7673897f5f91d
AndFroSwe <gummianka88@gmail.com>
2015-09-10 18:51:32 +0200

    Added better comments

--- main.py
+++ main.py
@@ -4,20 +4,23 @@ import drawtree as dw
 import random
 
 def print_average_gains(av_gain, av_gain_name = "", print_range = 6):
-        " Prints with nice format"
+        #Prints with nice format
         print(av_gain_name, end = "\t")
         for i in range(print_range):
                 print(round(av_gain[i], 4), end = "\t")
         print("")
 
 def draw_tree(dataset):
-        "Draws a decision tree from dataset "
+        #Draws a decision tree from dataset
         t = dt.buildTree(dataset, m.attributes)
         dw.drawTree(t)
         return t
 
 def check_tree_performance(tree, testset):
-        "Builds a dec tree from dataset and checks the performance on test set"
+        '''
+        Builds a dec tree from dataset and 
+        checks the performance on test set
+        '''
         performance_test = dt.check(tree, testset)
         return performance_test
 
@@ -28,7 +31,7 @@ def partition(data, fraction):
         return ldata[:breakPoint], ldata[breakPoint:]
 
 def print_table(row_names, col_names, data):
-        "Print a table with arbitrary number of columns and one row"
+        #Print a table with arbitrary number of columns and one row
         print("\t\t\t", end = "")
         for i in col_names:
                 print(i, end = "\t")
@@ -41,6 +44,10 @@ def print_table(row_names, col_names, data):
         print("")
 
 def make_pruned(dataset, testset, ratio = 0.5):
+        '''
+        Takes data- and testset and partitions data. 
+        Then makes pruned tree and checks performance
+        '''
         test, val = partition(dataset, ratio)
         tree = dt.buildTree(test, m.attributes)
         per_ref = check_tree_performance(tree, testset)
@@ -49,6 +56,7 @@ def make_pruned(dataset, testset, ratio = 0.5):
         return best, per_ref, per_pruned
 
 def prune(tree, testdata, performance_ref):
+        #Prunes tree from given test data
         alternatives = dt.allPruned(tree)
         best_per = 0
         best_tree = None
@@ -92,6 +100,8 @@ print_average_gains(M3, "M3")
 
 ## Assignment 3: Build decision trees of all data sets
 print("Assignment 3")
+
+# Build all trees and check performance
 monk1_tree = dt.buildTree(m.monk1, m.attributes)
 monk1_performance = check_tree_performance(monk1_tree, m.monk1test)
 monk1_train_per = check_tree_performance(monk1_tree, m.monk1)
@@ -115,6 +125,8 @@ print_table(["Performance Train", "Performance Test"],
 lbest = []
 lper_ref = []
 lper_pruned = []
+
+# Loop gets pruned trees for all 3 data sets
 for i in [(m.monk1, m.monk1test), (m.monk2, m.monk2test), (m.monk3, m.monk3test) ]:
         best, per_ref, per_pruned = make_pruned(i[0], i[1], 0.5)
         lbest.append(best)
